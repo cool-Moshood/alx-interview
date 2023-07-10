438bd56a75719d8879f2f110ebad0f1dfe869ba5
cool-Moshood <moshoodage@gmail.com>
2023-07-10 02:09:45 +0100

    task done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" pascal triangle """
+
+
+def pascal_triangle(n):
+    triangle = []
+    for row in range(n):
+        current_row = []
+        for col in range(row + 1):
+            if col == 0 or col == row:
+                current_row.append(1)
+            else:
+                current_row.append(triangle[row - 1][col - 1] + triangle[row - 1][col])
+        triangle.append(current_row)
+    return triangle
