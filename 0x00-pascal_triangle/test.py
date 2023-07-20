87fdf4bacb02ff557b1ce05abb18199e7947dfd0
cool-Moshood <moshoodage@gmail.com>
2023-07-20 21:43:50 +0100

    task

--- 0x00-pascal_triangle/test.py
+++ 0x00-pascal_triangle/test.py
@@ -1,18 +1,19 @@
-def minOperations(n: int) -> int:
-    if n <= 1 or n == float('inf'):
-        return 0
+def minOperations(n):
+    num_operations = 0
+    num_chars_in_file = 1
+    num_chars_in_clipboard = 0
 
-    operations = [float('inf')] * (n + 1)
-    operations[1] = 0
+    while num_chars_in_file < n:
+        if n % num_chars_in_file == 0:
+            # If the remaining characters to paste is divisible by the current file length,
+            # we can copy the entire file and paste it multiple times.
+            num_chars_in_clipboard = num_chars_in_file
+            num_operations += 1
 
-    for i in range(2, n + 1):
-        if n % i == 0:
-            operations[i] = i
+        num_chars_in_file += num_chars_in_clipboard
+        num_operations += 1
 
-        for j in range(1, i // 2 + 1):
-            operations[i] = min(operations[i], operations[j] + operations[i - j])
-
-    return operations[n]
+    return num_operations if num_chars_in_file == n else 0
 
 
 n = 4
--- 0x00-pascal_triangle/test2.py
+++ 0x00-pascal_triangle/test2.py
@@ -1,21 +1,45 @@
-#!/usr/bin/env python3
-""" an asynchronous coroutine that takes in
-an integer argument"""
-import random
-import asyncio
-from typing import List
-
-wait_random = __import__('0-basic_async_syntax').wait_random
-
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """return a list of values in async"""
-    sec_list = []
-    for _ in range(0, n):
-        sec_list.append(wait_random(max_delay))
-        srt = await asyncio.gather(*sec_list)
-        return sorted(srt)
+#!/usr/bin/python3
+"""module for minimum copy pase operations"""
 
 
-print(asyncio.run(wait_n(5, 5)))
-print(asyncio.run(wait_n(10, 7)))
-print(asyncio.run(wait_n(10, 0)))
+def minOperations(n):
+    """function for minimum copy paste operations"""
+    num_chars_in_file = 1
+    num_chars_in_clipboard = 0
+    num_operations = 0
+
+    while num_chars_in_file < n:
+        if num_chars_in_clipboard == 0:
+            num_chars_in_clipboard = num_chars_in_file
+            num_operations += 1
+
+        if num_chars_in_file == 1:
+            num_chars_in_file += num_chars_in_clipboard
+            num_operations += 1
+            continue
+
+        remaining_chars_to_paste = n - num_chars_in_file
+
+        if remaining_chars_to_paste < num_chars_in_clipboard:
+            return 0
+
+        if remaining_chars_to_paste % num_chars_in_file != 0:
+            num_chars_in_file += num_chars_in_clipboard
+            num_operations += 1
+        else:
+            num_chars_in_clipboard = num_chars_in_file
+            num_chars_in_file += num_chars_in_clipboard
+            num_operations += 2
+
+    if num_chars_in_file == n:
+        return num_operations
+    else:
+        return 0
+
+
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
