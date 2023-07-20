dc7dfccce2ccf56c06a8af15ca913216feb06d1e
cool-Moshood <moshoodage@gmail.com>
2023-07-20 22:21:36 +0100

    task done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,39 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
-
-
+"""Log parsing module"""
 import sys
 import re
 
 
-def parse_log_line(line):
-    """Parse a log line and return the status code and file size"""
-    pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)$'
-    match = re.match(pattern, line)
-    if match:
-        status_code = int(match.group(3))
-        file_size = int(match.group(4))
-        return status_code, file_size
-    return None, None
-
-
-def print_statistics(file_details):
-    """Print the current statistics"""
-    print("File size: {}".format(file_details['size']))
-    for key, value in sorted(file_details['codes'].items(), key=lambda x: int(x[0])):
-        if value != 0:
-            print("{}: {}".format(key, value))
-
-
 if __name__ == '__main__':
+    """Log parsing function"""
+    pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s{1}-\s\[\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{6}\]\s"GET\s\/projects\/260\sHTTP\/1.1"\s(\d{3})\s(\d+)$'  # nopep8
+
     file_details = {'codes': {'200': 0, '301': 0, '400': 0,
-                              '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}, 'size': 0}
+                              '401': 0, '403': 0, '404': 0,
+                              '405': 0,  '500': 0}, 'size': 0}
     possible_codes = [200, 301, 400, 401, 403, 404, 405, 500]
     count = 0
-
     try:
-        for line in sys.stdin:
+        for x in sys.stdin:
             count += 1
-            status_code, file_size = parse_log_line(line.strip())
-            if status_code is not None and file_size is not None and status_code in possible_codes:
-                file_details['codes'][str(status_code)] += 1
-                file_details['size'] += file_size
+            pattern = re.compile(pattern)
+            match = re.match(pattern, str(x.strip()))
+            if match:
 
-            if count % 10 == 0:
-                print_statistics(file_details)
+                if int(match.group(1)) in possible_codes:
+                    file_details['codes'][str(match.group(1))] += 1
+                    file_details['size'] = file_details['size'] + \
+                        int(match.group(2))
 
-    except KeyboardInterrupt:
-        pass
-
+                if count % 10 == 0:
+                    print("File size: {}".format(file_details['size']))
+                    for key, value in sorted(file_details['codes'].items(),
+                                             key=lambda x: int(x[0])):
+                        if value != 0:
+                            print("{}: {}".format(key, value))
     finally:
-        print_statistics(file_details)
+        print("File size: {}".format(file_details['size']))
+        for key, value in sorted(file_details['codes'].items(),
+                                 key=lambda x: int(x[0])):
+            if value != 0:
+                print("{}: {}".format(key, value))
