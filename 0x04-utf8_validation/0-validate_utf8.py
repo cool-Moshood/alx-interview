f786c953703bf1e495f2cff4bdca8de31037fc22
cool-Moshood <moshoodage@gmail.com>
2023-07-31 16:22:44 +0100

    task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a method that determines if a given data
+set represents a valid UTF-8 encoding"""
+
+
+def validUTF8(data):
+    """Determine the number of bytes for the current character
+    Check that the next bytes have the pattern '10xxxxxx'
+    """
+    num_bytes_to_read = 0
+    for byte in data:
+        if num_bytes_to_read == 0:
+            if byte >> 7 == 0b0:
+                num_bytes_to_read = 0
+            elif byte >> 5 == 0b110:
+                num_bytes_to_read = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes_to_read = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes_to_read = 3
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            num_bytes_to_read -= 1
+
+    return num_bytes_to_read == 0
