e1c81b11247c2836bfb7aeecc0d4056a8dd1ae39
cool-Moshood <moshoodage@gmail.com>
2023-08-05 13:51:02 +0100

    task hotfix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """ A project on queens puzzle chess games with two queens"""
 
-
 import sys
 
 
 def generate_solutions(row, column):
     """Generate all possible solutions for placing queens on the chessboard."""
-
     if row == 0:
         return [[]]
     prev_solutions = generate_solutions(row - 1, column)
@@ -21,21 +19,24 @@ def generate_solutions(row, column):
 
 def is_safe(q, x, array):
     """Check if it is safe to place a queen in the given position."""
-
     return all(x != col and abs(q - row) != abs(x - col)
                for row, col in enumerate(array))
 
 
 def n_queens():
     """Main function to handle user input and print solutions."""
-
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    N = int(sys.argv[1])
-    if N < 4:
-        print("N must be at least 4")
+    try:
+        N = int(sys.argv[1])
+        if N < 4:
+            print("Usage: nqueens N")
+            sys.exit(1)
+    except ValueError:
+        print("N must be a number")
         sys.exit(1)
+
     solutions = generate_solutions(N, N)
     for sol in solutions:
         print([[row, col] for row, col in enumerate(sol)])
