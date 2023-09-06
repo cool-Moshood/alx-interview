38927457b7017aeb5486c176258da7e3d59926e2
cool-Moshood <moshoodage@gmail.com>
2023-09-06 06:33:24 +0100

    task done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+"""Module that determines the fewest number of
+coins needed to meet a given amount total"""
+
 
 def makeChange(coins, total):
+    """function that performs action of module"""
+    total_count = 0
+    coins.sort(reverse=True)
 
+    if total <= 0:
+        return 0
     for x in coins:
-
-        n = x
-        m = n + x
-        # n = n + 1
-        print(m)
-
-
-print(makeChange([1, 2, 25], 37))
+        counter = total // x
+        total = total % x
+        total_count += counter
+        if total == 0:
+            return total_count
+    return -1
